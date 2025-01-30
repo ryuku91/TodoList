fa6931e887b09ece700ceba8e60dd01826936c18
鈴木琉久 <suzukiryukyuu@suzukiryukyuunoMacBook-Air.local>
2025-01-30 23:23:11 +0900

    valueの値の簡易化と空文字対策

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -2,32 +2,31 @@ import { useState } from "react";
 import "./style/display.css"
 const Display = ({vals, setVals}) => {
     const [sort, setSort] = useState(1);
+
     const handleCheck = (index) => {
     const newVals = [...vals];
     newVals[index].checked = !newVals[index].checked;
     setVals(newVals);
    }
+
    const sortChange = (e) => {
-    const sortValue = e.target.value;
-    if(sortValue === "createTime"){
-        setSort(1);
-    } else if (sortValue === "priority"){
-        setSort(2);
-    }
+    setSort(Number(e.target.value));
    }
+
    const sortedVals = [...vals].sort((a,b) => {
     if(sort === 1){
-        return vals.indexOf(a) - vals.indexOf(b);
+        return 0;
     } else if (sort === 2){
         return a.priority - b.priority;
     }
     return 0;
-   })
+   });
+
    return (
     <>
-    <select name="sort" onChange={sortChange}>
-        <option value="createTime">作成時</option>
-        <option value="priority">優先順</option>
+    <select name="sort" value={sort} onChange={sortChange}>
+        <option value={1}>作成時</option>
+        <option value={2}>優先順</option>
     </select>
     <ul className="checkbox-list">
       {sortedVals.map((item, index) => (
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -3,33 +3,30 @@ import "./style/input.css"
 const Input = ({val, setVal, vals, setVals}) => {
     const [priority, setPriority] = useState(1);
     const addButton = () => {
-        
+        if(val.trim() ===""){
+            alert("値を入力してください");
+            return;
+        }
         setVals([...vals, { text: val, checked: false, priority}]);
         setVal("");
     }
     const selectChange = (e) => {
-        const priorityValue = e.target.value;
-
-        if(priorityValue === "priority1"){
-            setPriority(1);
-        } else if (priorityValue === "priority2"){
-            setPriority(2);
-        } else if (priorityValue === "priority3")
-            setPriority(3);
+        setPriority(Number(e.target.value));
     }
     
     return (
     <div className="input-container">
     <input 
     type="text" 
+    value={val || ""}
     onChange={(e)=> setVal(e.target.value)}
     />
     <button onClick={addButton}>追加</button>
     <div className="container">
-    <select name="priorityA" onChange={selectChange}>
-        <option value="priority1">大</option>
-        <option value="priority2">中</option>
-        <option value="priority3">小</option>
+    <select name="priorityA" value={priority} onChange={selectChange}>
+        <option value={1}>大</option>
+        <option value={2}>中</option>
+        <option value={3}>小</option>
     </select>
         </div>
     </div> 
